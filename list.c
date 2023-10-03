984e747d2011654b3216b24a8205b5a9de533133
linzhenghao233 <1392872316@qq.com>
2023-10-03 22:22:18 +0800

    1003

--- list.c
+++ list.c
@@ -45,4 +45,48 @@ unsigned int ListItemCount(const List* plist) {
 bool AddItem(Item item, List* plist) {
 	Node* pnew;
 	Node* scan = *plist;
+
+	pnew = (Node*)malloc(sizeof(Node));
+	if (pnew = NULL)
+		return false;
+
+	CopyToNode(item, pnew);
+	pnew->next = NULL;
+	if (scan == NULL)
+		*plist = pnew;
+	else {
+		while (scan->next != NULL)
+			scan = scan->next;
+		scan->next = pnew;
+	}
+
+	return true;
+}
+
+/* 访问每个节点并执行pfun指向的函数 */
+void Traverse(const List* plist, void(*pfun)(Item item)) {
+	Node* pnode = *plist;
+
+	while (pnode != NULL) {
+		(*pfun)(pnode->item);
+		pnode = pnode->next;
+	}
+}
+
+/* 释放由malloc()分配的内存 */
+/* 设置链表指针为NULL */
+void EmptyTheList(List* plist) {
+	Node* psave;
+
+	while (*plist != NULL) {
+		psave = (*plst)->next;
+		free(*plist);
+		*plist = psave;
+	}
+}
+
+/* 局部函数定义 */
+/* 把一个项拷贝到节点中 */
+static void CopyToNode(Item item, Node* pnode) {
+	pnode->item = item;
 }
